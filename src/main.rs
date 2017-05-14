c84309f31018bbd3a55eddfdae17d293e0c24bff
Richard McCormack <brick@brick.codes>
2017-05-14 19:50:07 -0400

    Flag LCD as on during boot

--- src/main.rs
+++ src/main.rs
@@ -46,6 +46,7 @@ fn main() {
     let mut memory = memory::Memory::new();
     let mut cpu = cpu::CPU::new();
     let mut ppu = ppu::PPU::new();
+    memory.memory[0xff40] &= 0x80; // Flag LCD as on
     let rom_path = std::env::args().nth(1).expect("Gameboy ROM expected as argument");
 
     rom::load_rom(&mut memory, &rom_path).unwrap();
